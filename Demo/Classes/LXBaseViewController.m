5c2a646e089abed5a5d0522654c96ab7c528529a
mighty <953489948@qq.com>
2020-06-09 10:21:53 +0800

    添加控制器
    Signed-off-by: mighty <953489948@qq.com>

---
+++ Demo/Classes/LXBaseViewController.m
@@ -0,0 +1,32 @@
+//
+//  LXBaseViewController.m
+//  Demo
+//
+//  Created by MightLee on 2020/6/9.
+//
+
+#import "LXBaseViewController.h"
+
+@interface LXBaseViewController ()
+
+@end
+
+@implementation LXBaseViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    NSLog(@"Demo Test");
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
